502b8e611e4c359f8acc8452c97f419b06b48619
lukew244 <luke.webster86@gmail.com>
2016-09-20 12:44:07 +0100

    generates random ship locations

--- src/game.js
+++ src/game.js
@@ -10,8 +10,21 @@ Game.prototype.create = function() {
 };
 
 Game.prototype.plotShips = function() {
+  game = this;
   for(i = 0; i < this.ships.length; i++) {
     var ship = this.ships[i];
+    game.generateShipPosition(ship);
     this.map.array[ship.position[0]][ship.position[1]] = 'x';
   }
+};
+
+Game.prototype.generateShipPosition = function(ship) {
+  ship.position[0] = this.generateCoordinate();
+  ship.position[1] = this.generateCoordinate();
+  console.log(ship.position);
+};
+
+Game.prototype.generateCoordinate = function() {
+  return Math.floor((Math.random() * 10));
+
 };
